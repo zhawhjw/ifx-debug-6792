3e762086b5ddef23ed432291c49ad260eb75a712
Yunhao Zhang <yzhang22@78-4f-43-7b-1-b2.dhcp.lbnl.us>
2023-08-02 11:14:30 -0700

    add mac build instruction but ifx not supported yet

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,19 @@
-./fpm-0.9.0-linux-x86_64 build --compiler ifx
+#!/usr/bin/env bash
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     
+        machine=Linux
+        ./fpm-0.9.0-linux-x86_64 build --compiler ifx
+        ;;
+    Darwin*)    
+        machine=Mac
+        ./fpm-0.9.0-macos-x86_64 build --compiler ifx 
+        ;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    MSYS_NT*)   machine=Git;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+#./fpm-0.9.0-linux-x86_64 build --compiler ifx
